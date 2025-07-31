7ee4631829a33e3bd19c776b8f28e16653ac21ce
longtran22092009 <minhlong22092009@gmail.com>
2025-07-31 20:03:25 +0700

    Update Sorting and Searching section

---
+++ Sorting_and_Searching/058. Movie Festival II.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+//====== BITWISE ======//
+#define MASK(i) (1LL << (i))
+#define BIT(x, i) (1LL & ((x) >> (i)))
+#define ON(x, i) ((x) | MASK(i))
+#define OFF(x, i) ((x) & ~MASK(i))
+#define LASTBIT(mask) ((mask) & -(mask))
+#define SUBMASK(sub, mask) for (int sub = (mask); sub >= 1; sub = (sub - 1) & (mask))
+//====== OTHER ======//
+#define fi first
+#define se second
+#define ll long long
+#define endl '\n'
+#define sz(x) (int)size(x)
+#define all(x) begin(x), end(x)
+#define mod(x, k) ((((x) % (k)) + (k)) % (k))
+#define compress(c) sort(all(c)); c.erase(unique(all(c)), c.end());
+#define Longgggg ios_base::sync_with_stdio(0); cin.tie(0);
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+//====== FILE ======//
+#define IN "A.in"
+#define OUT "A.out"
+#define DEBUG "debug.out"
+//==================//
+
+const int INF = (int) 1e9+5;
+const ll LINF = (ll) 1e18;
+const ll MOD = (ll) 1e9+7;
+const int mxN = 100005;
+
+void solve() {
+    int n, k; cin >> n >> k;
+
+    vector <pair <int, int>> a(n);
+    FOR(i, 0, n - 1) {
+        int l, r; cin >> l >> r;
+        a[i] = {r, l};
+    }
+    sort(all(a));
+
+    multiset <int> ends;
+    int ans = 0;
+    for (auto &x : a) {
+        int st = x.se, ed = x.fi;
+        auto it = ends.upper_bound(st);
+        if (it != ends.begin()) {
+            --it; // *it: endTime of old movies <= ed.se: startTime of new movies
+            ends.erase(it);
+            ends.insert(ed);
+            ++ans;
+        } 
+        else if (sz(ends) < k) {
+            ends.insert(ed);
+            ++ans;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+signed main() {
+    if (fopen(IN, "r")) {
+        freopen(IN, "r", stdin);
+        freopen(OUT, "w", stdout);
+        freopen(DEBUG, "w", stderr);
+    }
+    Longgggg
+
+    ll t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
