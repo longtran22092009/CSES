7765a6caab696481386d0ad90bbf461f593604a6
longtran22092009 <minhlong22092009@gmail.com>
2025-08-07 20:58:15 +0700

    Update Graph Algorithms

---
+++ Graph Algorithms/111. De Bruijn Sequence.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+//====== BITWISE ======//
+#define MASK(i) (1LL << (i))
+#define BIT(x, i) (((x) >> (i)) & 1)
+#define ON(x, i) ((x) | MASK(i))
+#define OFF(x, i) ((x) & ~MASK(i))
+#define LASTBIT(mask) ((mask) & -(mask))
+#define SUBMASK(sub, mask) for (int sub = (mask); sub >= 1; sub = (sub - 1) & (mask))
+//====== OTHER ======//
+#define fi first
+#define se second
+#define ll long long
+#define endl '\n'
+#define sz(x) (int)size(x)
+#define all(x) begin(x), end(x)
+#define mod(x, k) ((((x) % (k)) + (k)) % (k))
+#define compress(c) sort(all(c)); c.erase(unique(all(c)), c.end());
+#define Longgggg ios_base::sync_with_stdio(0); cin.tie(0);
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+//====== FILE ======//
+#define IN "A.in"
+#define OUT "A.out"
+#define DEBUG "debug.out"
+//==================//
+
+const int INF = (int) 1e9+5;
+const ll LINF = (ll) 1e18;
+const ll MOD = (ll) 1e9+7;
+const int mxN = 15;
+
+string ans;
+map <string, vector <char>> adj;
+
+void dfs(string u) {
+    while (!adj[u].empty()) {
+        char ch = adj[u].back();
+        adj[u].pop_back();
+        dfs(u.substr(1) + ch);
+        ans += ch;
+    }
+}
+
+void solve() {
+    int n; cin >> n;
+
+    if (n == 1) {
+        cout << "10\n";
+        return;
+    }
+    
+    string start(n-1, '0');
+    FOR(i, 0, MASK(n-1)-1) {
+        string node = bitset<15>(i).to_string().substr(15 - (n-1));
+        adj[node].push_back('0');
+        adj[node].push_back('1');
+    }
+
+    dfs(start);
+    reverse(all(ans));
+    cout << start << ans << endl;
+}
+
+signed main() {
+    if (fopen(IN, "r")) {
+        freopen(IN, "r", stdin);
+        freopen(OUT, "w", stdout);
+        freopen(DEBUG, "w", stderr);
+    }
+    Longgggg
+
+    ll t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
